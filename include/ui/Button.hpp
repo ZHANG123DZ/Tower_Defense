507df9a2bd7368393a58415e1a5b2ccb56c4b3a5
zhang <hoangquangthang999@gmail.com>
2025-10-02 10:47:37 +0700

    hi

---
+++ include/ui/Button.hpp
@@ -0,0 +1,19 @@
+// #pragma once
+
+// #include <iostream>
+// #include <functional>
+// #include <SDL2/SDL.h>
+// #include <SDL2/SDL_image.h>
+
+// class Button {
+// private:
+//     int x, y;
+//     int borderRadius;
+//     SDL_Color borderColor, textColor, bgColor;
+//     SDL_Rect* rect;
+//     SDL_Texture* texture;  
+// public:
+//     setColor();
+//     onClick()
+//     onHover();
+// };
